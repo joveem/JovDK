8ec3a5a152344bd58744f525b57d7fa2ad3cd454
joveem <umjoveem@gmail.com>
2023-01-10 00:27:17 -0300

    adding PopUpManager

---
+++ Scripts/UI/PopUps/PopUpManager/Controller/Controller.cs
@@ -0,0 +1,55 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Bson;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+public partial class PopUpManager : MonoBehaviour
+{
+    void ShowPopUpInformation(
+        string title = null,
+        string description = null,
+        string positiveButtonText = null,
+        Action positiveCallback = null)
+    {
+
+        PopUp popUpInstance = Instantiate(_informationPopUpPrefab, _popUpContainer);
+
+        popUpInstance.SetTexts(title, description);
+        popUpInstance.SetButtonsText(positiveButtonText);
+        positiveCallback.DoIfNotNull(() => popUpInstance.SetConfirmationAction(positiveCallback));
+
+    }
+
+    void ShowPopUpConfirmation(
+        string title = null,
+        string description = null,
+        string positiveButtonText = null,
+        string negativeButtonText = null,
+        Action positiveCallback = null,
+        Action negativeCallback = null)
+    {
+
+        PopUp popUpInstance = Instantiate(_confirmationPopUpPrefab, _popUpContainer);
+
+        popUpInstance.SetTexts(title, description);
+        positiveCallback.DoIfNotNull(() => popUpInstance.SetConfirmationAction(positiveCallback));
+        negativeCallback.DoIfNotNull(() => popUpInstance.SetCancelAction(positiveCallback));
+
+    }
+}
---
+++ Scripts/UI/PopUps/PopUpManager/PopUpManager.cs
@@ -0,0 +1,66 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Bson;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+public partial class PopUpManager : MonoBehaviour
+{
+
+    [Space(5), Header("[ Dependencies ]"), Space(10)]
+
+    bool _dependencies;
+
+
+    [Space(5), Header("[ State ]"), Space(10)]
+
+    bool _state;
+
+
+    [Space(5), Header("[ Parts ]"), Space(10)]
+
+    [SerializeField] Transform _popUpContainer;
+
+
+    [Space(5), Header("[ Configs ]"), Space(10)]
+
+    [SerializeField] PopUp _informationPopUpPrefab;
+    [SerializeField] PopUp _confirmationPopUpPrefab;
+
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+}

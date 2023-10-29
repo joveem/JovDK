8515d9b65ffd965482e9679af4ecb7848951445a
joveem <umjoveem@gmail.com>
2023-10-29 02:49:09 -0300

    adding AxysDebugView

---
+++ Features/Debug - Input/AxysDebugView/AxysDebugView(Controller).cs
@@ -0,0 +1,54 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Bson;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+namespace JovDK.Debug.Input
+{
+    public partial class AxysDebugView : MonoBehaviour
+    {
+        void SetInitialValues()
+        {
+            _initialContainerSize = _axysContainerRect.sizeDelta;
+        }
+
+        public void SetAxysFactor(float axysFactor)
+        {
+            _axysFactor = axysFactor;
+            _factorText.text = "factor=" + _axysFactor.ToString("0.##");
+        }
+
+        public void SetInputValue(Vector3 inputValue)
+        {
+            _positionText.text =
+                "x=" + inputValue.x.ToString("0.##") + "\n" +
+                "y=" + inputValue.y.ToString("0.##");
+
+            Vector3 finalPosition = inputValue / _axysFactor;
+
+            finalPosition =
+                new Vector3(
+                    finalPosition.x * (_initialContainerSize.x / 2f),
+                    finalPosition.y * (_initialContainerSize.y / 2f),
+                    finalPosition.z * (_initialContainerSize.z / 2f));
+
+            _axysIndicatorRect.localPosition = finalPosition;
+        }
+    }
+}
---
+++ Features/Debug - Input/AxysDebugView/AxysDebugView.cs
@@ -0,0 +1,71 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Bson;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+namespace JovDK.Debug.Input
+{
+    public partial class AxysDebugView : MonoBehaviour
+    {
+
+        // [Space(5), Header("[ Dependencies ]"), Space(10)]
+
+        // bool _dependencies;
+
+
+        [Space(5), Header("[ State ]"), Space(10)]
+
+        float _axysFactor;
+        [SerializeField] Vector3 _initialContainerSize = new Vector3(150, 150);
+
+
+        [Space(5), Header("[ Parts ]"), Space(10)]
+
+        [SerializeField] RectTransform _axysContainerRect;
+        [SerializeField] RectTransform _axysIndicatorRect;
+        [SerializeField] TextMeshProUGUI _positionText;
+        [SerializeField] TextMeshProUGUI _factorText;
+
+
+        // [Space(5), Header("[ Configs ]"), Space(10)]
+
+        // bool _configs;
+
+
+        // void Awake()
+        // {
+
+        // }
+
+        void Start()
+        {
+            SetInitialValues();
+        }
+
+        // void Update()
+        // {
+
+        // }
+
+        // void FixedUpdate()
+        // {
+
+        // }
+    }
+}

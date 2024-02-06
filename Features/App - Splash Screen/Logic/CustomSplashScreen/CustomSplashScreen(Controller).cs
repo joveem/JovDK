3cc2b040fc2c12a9f28ccd392575d2c63037150f
joveem <umjoveem@gmail.com>
2024-02-06 02:14:02 -0300

    adding CustomSplashScreen to "Features/App - Splash Screen"

---
+++ Features/App - Splash Screen/Logic/CustomSplashScreen/CustomSplashScreen(Controller).cs
@@ -0,0 +1,60 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnitySplashScreen = UnityEngine.Rendering.SplashScreen;
+
+// third
+using TMPro;
+using DG.Tweening;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+namespace JovDK.App.SplashScreen
+{
+    public partial class CustomSplashScreen : MonoBehaviour
+    {
+
+
+        void ShowLogo()
+        {
+            _logoImage.SetActiveIfNotNull(true);
+        }
+
+        void HideLogo()
+        {
+            _logoImage.SetActiveIfNotNull(false);
+        }
+
+        void ShowContent()
+        {
+            _contentCanvasGroup.DoIfNotNull(() =>
+            {
+                _contentCanvasGroup.gameObject.SetActive(true);
+                _contentCanvasGroup.alpha = 1;
+            });
+        }
+
+        void PlayHideContent()
+        {
+            _contentCanvasGroup.DoIfNotNull(() =>
+            {
+                Tween tween = _contentCanvasGroup.DOFade(0, _hideFadeDuration);
+
+                TweenCallback onFinishCallback = () => Destroy(gameObject);
+                tween.onComplete += onFinishCallback;
+            });
+        }
+    }
+}
---
+++ Features/App - Splash Screen/Logic/CustomSplashScreen/CustomSplashScreen.cs
@@ -0,0 +1,91 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnitySplashScreen = UnityEngine.Rendering.SplashScreen;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+namespace JovDK.App.SplashScreen
+{
+    public partial class CustomSplashScreen : MonoBehaviour
+    {
+
+        // [Space(5), Header("[ Dependencies ]"), Space(10)]
+
+        // bool _dependencies;
+
+
+        // [Space(5), Header("[ State ]"), Space(10)]
+
+        // bool _state;
+
+
+        [Space(5), Header("[ Parts ]"), Space(10)]
+
+        [SerializeField] Image _logoImage;
+        [SerializeField] Image _backgroundImage;
+        [SerializeField] CanvasGroup _contentCanvasGroup;
+
+
+        [Space(5), Header("[ Configs ]"), Space(10)]
+
+        [SerializeField] float _splashDuration = 2f;
+        [SerializeField] float _hideFadeDuration = 1f;
+        [SerializeField] float _enableClickGapAfterFadeStart = 0.2f;
+        [SerializeField] bool _DEBUG_simulateUnitySplashOnEditor = true;
+
+
+        void Awake()
+        {
+            ShowContent();
+            HideLogo();
+        }
+
+        IEnumerator Start()
+        {
+#if UNITY_EDITOR
+            if (_DEBUG_simulateUnitySplashOnEditor)
+            {
+                UnitySplashScreen.Begin();
+                UnitySplashScreen.Draw();
+            }
+#endif
+
+            while (!UnitySplashScreen.isFinished)
+                yield return null;
+
+            ShowLogo();
+
+            yield return new WaitForSeconds(_splashDuration);
+            PlayHideContent();
+
+            yield return new WaitForSeconds(_enableClickGapAfterFadeStart);
+            _backgroundImage.raycastTarget = false;
+        }
+
+        // void Update()
+        // {
+
+        // }
+
+        // void FixedUpdate()
+        // {
+
+        // }
+    }
+}

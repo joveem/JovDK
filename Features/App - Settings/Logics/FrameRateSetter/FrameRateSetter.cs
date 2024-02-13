d42e04127ba8689234e32a5c84437f7ca477f002
joveem <umjoveem@gmail.com>
2024-02-13 16:11:16 -0300

    adding FrameRateSetter to "Features/App - Settings"

---
+++ Features/App - Settings/Logics/FrameRateSetter/FrameRateSetter.cs
@@ -0,0 +1,67 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+// third
+using TMPro;
+
+// from company
+using JovDK.Debug;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Json;
+
+// from project
+// ...
+
+
+namespace JovDK.App.Settings.Video
+{
+    public partial class FrameRateSetter : MonoBehaviour
+    {
+
+        // [Space(5), Header("[ Dependencies ]"), Space(10)]
+
+        // bool _dependencies;
+
+
+        // [Space(5), Header("[ State ]"), Space(10)]
+
+        // bool _state;
+
+
+        // [Space(5), Header("[ Parts ]"), Space(10)]
+
+        // bool _parts;
+
+
+        // [Space(5), Header("[ Configs ]"), Space(10)]
+
+        // bool _configs;
+
+
+        // void Awake()
+        // {
+
+        // }
+
+        void Start()
+        {
+            // TODO: REVIEW THIS!
+            Application.targetFrameRate = 60;
+        }
+
+        // void Update()
+        // {
+
+        // }
+
+        // void FixedUpdate()
+        // {
+
+        // }
+    }
+}

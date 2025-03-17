674a98483c446136cf93ba497ef4452115a6409a
joveem <umjoveem@gmail.com>
2025-03-17 02:27:26 -0300

    [ TO-REVIEW ] adding AudioService to "Features/Sound - Sound Service"

---
+++ Features/Sound - Sound Service/logic/AudioService.cs
@@ -0,0 +1,164 @@
+// system / unity
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using SystemRandom = System.Random;
+using UnityRandom = UnityEngine.Random;
+
+// third
+using DG.Tweening;
+using TMPro;
+
+// from company
+using JovDK.Debugging;
+using JovDK.SafeActions;
+using JovDK.SerializingTools.Json;
+using System.Linq;
+
+// from project
+// ...
+
+
+namespace JovDK.Audio.Service
+{
+    public partial class AudioService : MonoBehaviour
+    {
+
+        // [Space(5), Header("[ Dependencies ]"), Space(10)]
+
+        // [SerializeField] bool _dependencies;
+
+
+        [Space(5), Header("[ State ]"), Space(10)]
+
+        // [SerializeField] bool _state;
+        Dictionary<string, AudioConfig> _currentAudiosById = new Dictionary<string, AudioConfig>();
+
+
+        // [Space(5), Header("[ Parts ]"), Space(10)]
+
+        // [SerializeField] bool _parts;
+        // [SerializeField] Button _mainButton;
+        // [SerializeField] TextMeshProUGUI _mainText;
+        // [SerializeField] Image _mainImage;
+
+
+        [Space(5), Header("[ Configs ]"), Space(10)]
+
+        [SerializeField] List<AudioConfig> _audiosList = new List<AudioConfig>();
+
+
+
+        #region MonoBehaviour
+        void Awake()
+        {
+            SetInitialState();
+        }
+        #endregion MonoBehaviour
+
+        #region Controller
+        void SetInitialState()
+        {
+            _audiosList.DoIfNotNull(() =>
+            {
+                foreach (AudioConfig audioConfig in _audiosList)
+                {
+                    audioConfig.DoIfNotNull(() =>
+                    {
+                        bool isAlreadyRegistered = _currentAudiosById.ContainsKey(audioConfig.Id);
+
+                        if (!isAlreadyRegistered)
+                        {
+                            int variationsAmount = audioConfig.AudioClipsVariationsList.Length;
+                            audioConfig.AudioSourceIntances = new AudioSource[variationsAmount];
+
+                            for (int i = 0; i < variationsAmount; i++)
+                            {
+                                AudioClip audioClipVariation = audioConfig.AudioClipsVariationsList[i];
+
+                                audioClipVariation.DoIfNotNull(() =>
+                                {
+                                    AudioSource audioSourceIntance = gameObject.AddComponent<AudioSource>();
+                                    audioSourceIntance.clip = audioClipVariation;
+                                    audioSourceIntance.volume = audioConfig.VolumeFactor;
+                                    audioSourceIntance.pitch = audioConfig.PitchFactor;
+                                    audioSourceIntance.spatialBlend = audioConfig.Is2D ? 0f : 1f;
+
+                                    audioConfig.AudioSourceIntances[i] = audioSourceIntance;
+                                },
+                                () =>
+                                {
+                                    DebugExtension.DevLogError(
+                                        "audioClipVariation is null!" + "\n" +
+                                        "audioConfig.Id = " + audioConfig.Id.SerializeObjectToJSON() + "\n" +
+                                        "i = " + i + "\n" +
+                                        "variationsAmount = " + variationsAmount + "\n" +
+                                        "");
+                                });
+                            }
+
+                            _currentAudiosById[audioConfig.Id] = audioConfig;
+                        }
+                        else
+                        {
+                            DebugExtension.DevLogError(
+                                "$$ > ".ToColor(GoodColors.Red) +
+                                "Duplicated Id in _audiosList!".ToColor(GoodColors.Orange) + "\n" +
+                                "audioConfig.Id = " + audioConfig.Id.SerializeObjectToJSON() + "\n" +
+                                "");
+                        }
+                    });
+                }
+            });
+        }
+
+        public void _INTERNAL_PlaySfx(string sfxId)
+        {
+            bool isAlreadyRegistered = _currentAudiosById.ContainsKey(sfxId);
+
+            if (isAlreadyRegistered)
+            {
+                AudioConfig audioConfig = _currentAudiosById[sfxId];
+
+                int randomIndex = 0;
+                int variationsAmount = audioConfig.AudioClipsVariationsList.Length;
+
+                if (variationsAmount > 0)
+                    randomIndex = UnityRandom.Range(0, variationsAmount);
+
+                AudioSource audioSourceToPlay = audioConfig.AudioSourceIntances[randomIndex];
+                audioSourceToPlay.DoIfNotNull(() => audioSourceToPlay.PlayOneShot(audioSourceToPlay.clip));
+            }
+            else
+            {
+                DebugExtension.DevLogError(
+                    "Audio was not found!" + "\n" +
+                    "sfxId = " + sfxId.SerializeObjectToJSON() + "\n" +
+                    "");
+            }
+        }
+        #endregion Controller
+    }
+
+    [Serializable]
+    public class AudioConfig
+    {
+        public string Id = "UNDEFINED";
+        public AudioClip[] AudioClipsVariationsList;
+        public float VolumeFactor = 1f;
+        public float PitchFactor = 1f;
+        public bool Is2D = true;
+        [HideInInspector] public AudioSource[] AudioSourceIntances;
+    }
+
+    public static class AudioServiceExtension
+    {
+        public static void PlaySfx(this AudioService baseAudioService, string sfxId)
+        {
+            baseAudioService.DoIfNotNull(() => baseAudioService._INTERNAL_PlaySfx(sfxId));
+        }
+    }
+}

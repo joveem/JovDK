0f1f71176a5a9b13b17f977f7e9a4fb69f60db26
joveem <umjoveem@gmail.com>
2025-07-14 21:53:19 -0300

    disabling "no scripts" warning log in "JovDK.Monetization.AdMob" and "JovDK.Monetization.UnityAds" on "_Asmdefs"

---
+++ _Asmdefs/JovDK.Monetization.UnityAds/_DISABLE_HELPER_WarningLogs.cs
@@ -0,0 +1,21 @@
+// system / unity
+// ...
+
+// third
+// ...
+
+// from company
+// ...
+
+
+// from project
+// ...
+
+
+namespace PackageName.MajorContext.MinorContext
+{
+    public partial class _DISABLE_HELPER_WarningLogs
+    {
+
+    }
+}
